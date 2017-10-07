01fdf32c5e2479f6f2fc3754f4e86266020d0e08
Miranda\Edna Lopes <slipknot_14904@hotmail.com>
2017-10-07 12:53:55 -0300

    Modificações na página inicial e envio de 3 novas imagens.

--- views/HomeView.php
+++ views/HomeView.php
@@ -1,7 +1,5 @@
 <section>
-
-  <div class="row" >
-
+        
     <div class="slider">
       <ul class="slides">
         <li><img src="http://arquivos.tribunadonorte.com.br/fotos/152359.jpg"></li>
@@ -10,108 +8,165 @@
         <li><img src="http://www.riograndedonorte.net/wp-content/uploads/2014/09/festa_do_boi_2010_-_fotos-_ney_douglas_novo_jornal.jpg"></li>
       </ul>
     </div>
-
+      
+    <br/><div style="text-align:center">
+         <a class="btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Julgamento da raça Pardo-Suiço I, EMPARN e Convidados e +" style="background-color:midnightblue">Programação</a>
+         <a class="btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="7 a 14 de Outubro" style="background-color:midnightblue">Dia do Evento</a>
+         <a class="btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Parque Aristófanes Fernandes (Parnamirim/RN)" style="background-color:midnightblue">Local do Evento</a>
+         <a class="btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="ANORC e Gorveno do Rio Grande do Norte" style="background-color:midnightblue">Realização</a>
+    </div>  <br/>
+      
+    <div class="intro-message center-align">
+      <h5>Informações do evento</h5>
+      <hr class="intro-divider">
+    </div>
+      
+    <div class="row">
+        <div class="col s12 m4">
+            <div class="card" style="width=320px">
+                <div class="card-image">
+                     <img src="http://arquivos.tribunadonorte.com.br/fotos/152359.jpg">
+                     <span class="card-title">Festa do Boi</span>
+                     <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                </div>
+                <div class="card-content">
+                    <p>Informações gerais da gesta do boi.</p>
+                </div>
+            </div>
+        </div>
+          
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="http://www.al.rn.gov.br/portal/_ups/noticias/2015/10/02/7b8b0aa9b9789a6f1ae1292410700ee5.jpg">
+                    <span class="card-title">Programação</span>
+                    <a class="btn-floating halfway-fab waves-effect waves-light red" href="index.php?page=Programacao"><i class="material-icons">add</i></a>
+                </div>
+                <div class="card-content">
+                    <p>55ª Edição da Festa do Boi. O maior evento do setor agropecuário do nordeste e, uma das principais festas populares e feiras de negócios do Rio Grande do Norte. Fique por dentro de tudo que vai rolar clicando no botão (+) acima.</p>
+                </div>
+            </div>
+        </div>
+          
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="http://www.riograndedonorte.net/wp-content/uploads/2014/09/festa_do_boi_2010_-_fotos-_ney_douglas_novo_jornal.jpg">
+                    <span class="card-title">História do Evento</span>
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                </div>
+                <div class="card-content">
+                    <p>A Festa do Boi é um dos eventos mais antigos do Rio Grande do Norte e, agora em 2012, vai completar meio século de existência. A cada novo ano a festa cresce conquistando novos expositores e visitantes que se encantam com a diversidade de opções.</p>
+                </div>
+            </div>
+        </div>
+    </div> 
+        
     <div class="intro-message center-align">
       <h3>Parceiros</h3>
       <hr class="intro-divider">
     </div>
+    
+    <div class="row" >
+        <div class=" col  m3 s6 center-align">
+          <a href="#">
+            <img height="33px" src="resources/app/img/abcz.png" alt="">
+          </a>
+        </div>
 
-    <div class=" col  m3 s6 center-align">
-      <a href="#">
-        <img height="33px" src="resources/app/img/abcz.png" alt="">
-      </a>
-    </div>
+        <div class="col  m3 s6 center-align">
+          <a href="#">
+            <img height="70px" src="resources/app/img/agn.png" alt="">
+          </a>
+        </div>
 
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="70px" src="resources/app/img/agn.png" alt="">
-      </a>
-    </div>
+        <div class="col  m3 s6 center-align">
+          <a href="#">
+            <img height="80px" src="resources/app/img/ancoc.png" alt="">
+          </a>
+        </div>
 
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="80px" src="resources/app/img/ancoc.png" alt="">
-      </a>
-    </div>
+        <div class="col  m3 s6 center-align">
+          <a href="#">
+            <img height="80px" src="resources/app/img/anorc.png" alt="">
+          </a>
+        </div>
 
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="80px" src="resources/app/img/anorc.png" alt="">
-      </a>
-    </div>
+        <div class="col  m3 s6 center-align">
+          <a href="#">
+            <img height="80px" src="resources/app/img/assembleia.png" alt="">
+          </a>
+        </div>
 
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="80px" src="resources/app/img/assembleia.png" alt="">
-      </a>
-    </div>
+        <div class="col  m3 s6 center-align">
+          <a href="#">
+            <img height="80px" src="resources/app/img/bancobrasil.png" alt="">
+          </a>
+        </div>
 
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="80px" src="resources/app/img/bancobrasil.png" alt="">
-      </a>
-    </div>
+        <div class="col  m3 s6 center-align">
+          <a href="#">
+            <img height="60px" src="resources/app/img/banconordeste.png" alt="">
+          </a>
+        </div>
 
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="60px" src="resources/app/img/banconordeste.png" alt="">
-      </a>
-    </div>
+        <div class="col  m3 s6 center-align">
+          <a href="#">
+            <img height="80px" src="resources/app/img/emater.png" alt="">
+          </a>
+        </div>
 
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="80px" src="resources/app/img/emater.png" alt="">
-      </a>
-    </div>
+        <div class="col  m3 s6 center-align">
+          <a href="#">
+            <img height="80px" src="resources/app/img/emparn.png" alt="">
+          </a>
+        </div>
 
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="80px" src="resources/app/img/emparn.png" alt="">
-      </a>
-    </div>
+        <div class="col  m3 s6 center-align">
+          <a href="#">
+            <img height="80px" src="resources/app/img/governodorn.png" alt="">
+          </a>
+        </div>
 
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="80px" src="resources/app/img/governodorn.png" alt="">
-      </a>
-    </div>
+        <div class="col  m3 s6 center-align">
+          <a href="#">
+            <img height="55px" src="resources/app/img/governofederal.png" alt="">
+          </a>
+        </div>
 
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="55px" src="resources/app/img/governofederal.png" alt="">
-      </a>
-    </div>
+        <div class="col  m3 s6 center-align">
+          <a href="#">
+            <img height="80px" src="resources/app/img/idiarn.png" alt="">
+          </a>
+        </div>
 
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="80px" src="resources/app/img/idiarn.png" alt="">
-      </a>
-    </div>
+        <div class="col  m3 s6 center-align">
+          <a href="#">
+            <img height="100px" src="resources/app/img/prefeituraparnamirim.png" alt="">
+          </a>
+        </div>
 
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="100px" src="resources/app/img/prefeituraparnamirim.png" alt="">
-      </a>
-    </div>
+        <div class="col  m3 s6 center-align">
+          <a href="#">
+            <img height="70px" src="resources/app/img/sebrae.png" alt="">
+          </a>
+        </div>
 
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="70px" src="resources/app/img/sebrae.png" alt="">
-      </a>
-    </div>
-
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="80px" src="resources/app/img/sistemasenar.png" alt="">
-      </a>
-    </div>
-
-    <div class="col  m3 s6 center-align">
-      <a href="#">
-        <img height="100px" src="resources/app/img/ceres.png" alt="">
-      </a>
-    </div>
-
+        <div class="col  m3 s6 center-align">
+          <a href="#">
+            <img height="80px" src="resources/app/img/sistemasenar.png" alt="">
+          </a>
+        </div>
   <div/>
+  
+    <div class="row" style="text-align:center">
+        <div class="col s12">
+            <h5>Redes Sociais</h5>
+            <hr class="intro-divider"><br/>
+        </div>
+        <div class="col s4"><a class="btn btn-floating pulse" target="_blank" href="https://www.facebook.com/festadoboi/"><img height="40px" src="resources/app/img/facebook2.png" alt=""></a><P>FACEBOOK</P></div>
+        <div class="col s4"><a class="btn btn-floating pulse" target="_blank" href="https://www.instagram.com/anorc.rn/"><img height="41px" src="resources/app/img/instagram2.png" alt=""></a><P>INSTAGRAM</P></div>
+        <div class="col s4"><a class="btn btn-floating pulse" target="_blank" href="https://www.instagram.com/anorc.rn/"><img height="41px" src="resources/app/img/youtube2.png" alt=""></a><P>YOUTUBE</P></div>
+    </div>
 
 <section/>
